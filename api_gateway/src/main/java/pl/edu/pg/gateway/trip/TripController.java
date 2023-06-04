91c320a76fba565e39c0da81d9c648fd35e24fdc
Winetq <winetq9679@gmail.com>
2023-06-04 17:01:37 +0200

    Add a new endpoint: /api/trips/{id}

--- api_gateway/src/main/java/pl/edu/pg/gateway/trip/TripController.java
+++ api_gateway/src/main/java/pl/edu/pg/gateway/trip/TripController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.pg.gateway.trip.dto.TripDetailsResponse;
 import pl.edu.pg.gateway.trip.dto.TripsResponse;
 
 @RestController
@@ -23,6 +25,13 @@ class TripController {
     ResponseEntity<TripsResponse> getTrips() {
         final var maybeTrips = tripService.getTrips();
         return maybeTrips.map(tripsResponse -> new ResponseEntity<>(tripsResponse, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("{id}")
+    ResponseEntity<TripDetailsResponse> getTripDetails(@PathVariable Long id) {
+        final var maybeTrip = tripService.getTripDetails(id);
+        return maybeTrip.map(tripDetailsResponse -> new ResponseEntity<>(tripDetailsResponse, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
     
--- api_gateway/src/main/java/pl/edu/pg/gateway/trip/TripService.java
+++ api_gateway/src/main/java/pl/edu/pg/gateway/trip/TripService.java
@@ -1,12 +1,12 @@
 package pl.edu.pg.gateway.trip;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import pl.edu.pg.gateway.trip.dto.TripDetailsRequest;
+import pl.edu.pg.gateway.trip.dto.TripDetailsResponse;
 import pl.edu.pg.gateway.trip.dto.TripsRequest;
 import pl.edu.pg.gateway.trip.dto.TripsResponse;
 
@@ -14,21 +14,34 @@ import java.util.Optional;
 
 @Service
 class TripService {
-    private final Logger log = LoggerFactory.getLogger(TripService.class);
     private final RabbitTemplate rabbitTemplate;
     private final String getTripsQueueName;
+    private final String getTripDetailsQueueName;
 
     @Autowired
     TripService(final RabbitTemplate rabbitTemplate,
-                @Value("${spring.rabbitmq.queue.trip.get.all}") final String getTripsQueueName) {
+                @Value("${spring.rabbitmq.queue.trip.get.all}") final String getTripsQueueName,
+                @Value("${spring.rabbitmq.queue.trip.get.details}") final String getTripDetailsQueueName) {
         this.rabbitTemplate = rabbitTemplate;
         this.getTripsQueueName = getTripsQueueName;
+        this.getTripDetailsQueueName = getTripDetailsQueueName;
     }
 
     Optional<TripsResponse> getTrips() {
         final TripsRequest request = TripsRequest.builder().build();
         final TripsResponse response = rabbitTemplate.convertSendAndReceiveAsType(
                 getTripsQueueName,
+                request,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+        return Optional.ofNullable(response);
+    }
+
+    Optional<TripDetailsResponse> getTripDetails(Long id) {
+        final TripDetailsRequest request = TripDetailsRequest.builder().id(id).build();
+        final TripDetailsResponse response = rabbitTemplate.convertSendAndReceiveAsType(
+                getTripDetailsQueueName,
                 request,
                 new ParameterizedTypeReference<>() {
                 }
