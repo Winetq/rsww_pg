6c005998d4deb855cd6ebfff9437bba97ec1e550
Michał Błajet <michal.blajet00@gmail.com>
2023-06-12 13:36:40 +0200

    Fix id generation for hotels in command service

--- accommodation_service_commands/src/main/java/pl/edu/pg/accommodation/hotel/entity/HotelEntity.java
+++ accommodation_service_commands/src/main/java/pl/edu/pg/accommodation/hotel/entity/HotelEntity.java
@@ -26,7 +26,7 @@ import java.util.Objects;
 @AllArgsConstructor
 public class HotelEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
     private String name;
